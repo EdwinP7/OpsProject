e261af9ad350d103070798b8281ba054cb7ec290
EdwinP7 <edwinperea7@gmail.com>
2019-01-01 11:44:22 -0800

    Bonus: Add a contacts and contact detail page

--- accounting/views.py
+++ accounting/views.py
@@ -8,7 +8,7 @@ from utils import PolicyAccounting
 from accounting import app, db
 
 # Import our models
-from models import Contact, Invoice, Policy
+from models import Contact, Invoice, Policy, Payment
 
 # Routing for the server.
 @app.route("/")
@@ -19,10 +19,9 @@ def index():
 
 @app.route("/policies/")
 def policies():
-    """ Returns all policies """
+    """ View for policy search page """
 
-    policies = Policy.query.all()
-    return render_template('policies.html', policies=policies)
+    return render_template('policies.html')
 
 
 @app.route("/policy/", methods=['GET'])
@@ -35,16 +34,19 @@ def policy():
     if policy_id is None or policy_id == '':
         return Response(status=400)
 
+    # Default date
     if not date:
-        date = datetime.now()
+        date = datetime.now().date()
     else:
         date = datetime.strptime(date, '%Y-%m-%d').date()
 
+    # Return error page if policy not found
     try:
         policy_account = PolicyAccounting(policy_id)
     except NoResultFound as ex:
-        return render_template('error.html', status=404, )
+        return render_template('error.html', status=404, resource_type='Policy')
 
+    # Retrieve relevant information about policy for view
     policy_details = {
         'account_balance': policy_account.return_account_balance(date),
         'agent': policy_account.agent,
@@ -57,3 +59,31 @@ def policy():
     }
 
     return render_template('policy_detail.html', policy_details=policy_details)
+
+
+@app.route("/contacts/")
+def contacts():
+    """ View for contacts page """
+
+    contacts_list = Contact.query.all()
+
+    return render_template('contacts.html', contacts=contacts_list)
+
+@app.route("/contacts/<contact_id>/", methods=['GET'])
+def contact(contact_id):
+    """ View for contacts page """
+
+    try:
+        contact = Contact.query.filter_by(id=contact_id).one()
+    except:
+        return render_template('error.html', status=404, resource_type='Contact')
+
+    # Get contact's policy
+    contact.policy = Policy.query.filter_by(named_insured=contact.id).first()
+
+    # Get last payment
+    contact.last_payment = Payment.query.filter_by(contact_id=contact.id)\
+                                        .order_by(Payment.transaction_date.desc())\
+                                        .first()
+
+    return render_template('contact_detail.html', contact=contact)
